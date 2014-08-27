0b407b55bb560ad7b8684da4e9ed679cdb296244
Sean Smith <seansmith1020@gmail.com>
2014-08-27 10:04:28 -0600

    working code

---
+++ lib/line-break.rb
@@ -0,0 +1,37 @@
+require 'pp'
+require 'pry'
+
+def convert_file(file_out, file_in, length)
+  file = File.open(file_in, 'r')
+  content = file.read
+  file.close
+  new_content = content.dup
+
+  to_add = ""
+
+  until new_content == ""
+    string = new_content.slice!(0,20)
+    p "IS A SPACE? #{string[length-1]}"
+    if string[length-1] == " "
+      to_add << (string + "\n")
+    else
+      backward = string.reverse
+      space = backward.index(" ")
+      if space == nil
+        to_add << backward.reverse
+      else
+        word = backward.slice!(0,space)
+        new_content = word.reverse + new_content
+        to_add << (backward.reverse + "\n")
+      end
+    end
+  end
+
+  File.open(file_out, 'w'){|file|file.write(to_add)}
+
+end
+
+convert_file('/Users/seansmith/gSchoolWork/warmups/line-breaks/data/awesome-sauce-test.txt', '/Users/seansmith/gSchoolWork/warmups/line-breaks/data/awesome-sauce.txt', 20)
+convert_file('/Users/seansmith/gSchoolWork/warmups/line-breaks/data/I knew them before they were popular.txt','/Users/seansmith/gSchoolWork/warmups/line-breaks/data/I knew them before they were popular-OUTPUT.txt', 80)
+convert_file('/Users/seansmith/gSchoolWork/warmups/line-breaks/data/what!?.txt','/Users/seansmith/gSchoolWork/warmups/line-breaks/data/what!_-OUTPUT.txt', 20)
+convert_file('/Users/seansmith/gSchoolWork/warmups/line-breaks/data/test_out', '/Users/seansmith/gSchoolWork/warmups/line-breaks/data/test', 20)
